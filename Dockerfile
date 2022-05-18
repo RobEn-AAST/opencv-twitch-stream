7ae2b564b7043a897304c9d1a52d254dcda4c38d
mhwahdab <moustafawahdan55@gmail.com>
2022-05-18 21:47:02 +0200

    Added Docker Img

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ubuntu:latest
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt update -y
+
+RUN apt upgrade -y
+
+RUN apt install python3 -y
+
+RUN apt install python3-pip -y
+
+RUN pip3 install opencv-python
+
+RUN pip3 install python-twitch-stream
+
+RUN apt install ffmpeg -y
+
+RUN pip3 install rovlib
+
+WORKDIR /twitch
+
--- codes/stream.py
+++ codes/stream.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""
-This is a small example which creates a twitch stream to connect with
-and changes the color of the video according to the colors provided in
-the chat.
-"""
-from __future__ import print_function
 from twitchstream.outputvideo import TwitchBufferedOutputStream
 import argparse
 import numpy as np
 import cv2
 import time
+from rovlib.cameras import RovCam
 
-STREAM_FROM = 'vid.mp4'
+
+STREAM_FROM = 0
 
 
 def convert_to_twitch_frame(recieved_frame, resize=True, bgr_to_rgb=True):
@@ -25,11 +19,12 @@ def convert_to_twitch_frame(recieved_frame, resize=True, bgr_to_rgb=True):
         recieved_frame = cv2.cvtColor(recieved_frame, cv2.COLOR_BGR2RGB)
 
     output_frame = np.zeros((480, 640, 3), dtype=np.float32)    
-    output_frame = recieved_frame[:, :] / 255.0 
+    output_frame = recieved_frame[:, :] / 255.0
     return output_frame
 
 
 if __name__ == "__main__":
+    cam = RovCam(RovCam.FRONT)
     parser = argparse.ArgumentParser(description=__doc__)
     required = parser.add_argument_group('required arguments')
     required.add_argument('-s', '--streamkey',
@@ -45,15 +40,7 @@ if __name__ == "__main__":
             verbose=True,
             enable_audio=False) as videostream:
 
-        cap = cv2.VideoCapture(STREAM_FROM)
-
         while True:
             if videostream.get_video_frame_buffer_state() < 30:
-                ret, frame = cap.read() 
-                
-                if ret:
-                     videostream.send_video_frame(convert_to_twitch_frame(frame))
-                else:
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                    continue
-                time.sleep(0.008)
+                frame = cam.read()
+                videostream.send_video_frame(convert_to_twitch_frame(frame))
